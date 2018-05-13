2578c1150c731408ee66de501396a3d5a480be0b
Unknown <jason.m.grunfeld@gmail.com>
2018-05-13 13:59:10 -0500

    Added addition/subtraction switch
    Added addition/subtraction switch

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -13,11 +13,42 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var buttonCount = 0
-   
+    // var buttonCount = 0
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    @IBOutlet weak var AdditionSwitch: UISwitch!
+    
+    
     @IBAction func buttonTapped(_ sender: Any) { // make screen red and change messege to "You got it" when tapped
-       
-        buttonCount = buttonCount + 1
+       print(topTextField.text!)
+       print(bottomTextField.text!)
+        
+        
+        if AdditionSwitch.isOn {
+        
+       let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+        
+       myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+        
+        }
+        
+        else {
+            
+            
+            let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
+            
+            
+        }
+        
+        
+    }
+    
+        
+        /*  buttonCount += 1 // or..... buttonCount = buttonCount + 1
         
         print(buttonCount)
         
@@ -27,14 +58,12 @@ class ViewController: UIViewController {
             myLabel.text = "You freaking did it"
         }
         
-    }
+    } */
     
-    @IBOutlet weak var myLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         
     }
